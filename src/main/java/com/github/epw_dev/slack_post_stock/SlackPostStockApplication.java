d64e77cd25a7e3535f4492d8956d2f2bee5a5fd5
taqm <warabii.taqm@gmail.com>
2019-10-12 14:05:28 +0900

    generated project

---
+++ src/main/java/com/github/epw_dev/slack_post_stock/SlackPostStockApplication.java
@@ -0,0 +1,13 @@
+package com.github.epw_dev.slack_post_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackPostStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackPostStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/epw_dev/slack_post_stock/SlackPostStockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.epw_dev.slack_post_stock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlackPostStockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
